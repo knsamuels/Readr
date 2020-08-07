eba32ecbf4423db1b1a7985a19d06ca6d06326b8
Adetokunbo Babatunde <swiftytoks@gmail.com>
2020-08-07 18:09:31 -0400

    Updated BookController

--- Readr/Readr/Models/Bookclub.swift
+++ Readr/Readr/Models/Bookclub.swift
@@ -8,6 +8,22 @@
 
 import Foundation
 import UIKit
+import CloudKit
+
+struct BookclubConstants {
+    static let recordTypeKey = "Bookclub"
+    fileprivate static let nameKey = "name"
+    fileprivate static let descriptionKey = "description"
+    fileprivate static let adminKey = "admin"
+    fileprivate static let adminContactInfoKey = "adminContactInfo"
+    fileprivate static let membersKey = "members"
+    fileprivate static let profilePictureKey = "profilePicture"
+    fileprivate static let currentlyReadingKey = "currentlyReading"
+    fileprivate static let pastReadsKey = "pastReads"
+    fileprivate static let memberMessagesKey = "memberMessages"
+    fileprivate static let meetingInfoKey = "meetingInfo"
+    fileprivate static let memberCapacityKey = "memberCapacity"
+}
 
 class Bookclub {
     var name: String
@@ -31,7 +47,6 @@ class Bookclub {
         self.members = members
         self.description = description
         self.profilePicture = profilePicture
-        self.adminContactInfo = adminContactInfo
         self.currentlyReading = currentlyReading
         self.pastReads = pastReads
         self.memberMessages = memberMessages
@@ -39,3 +54,45 @@ class Bookclub {
         self.memberCapacity = memberCapacity
     }
 }
+
+extension CKRecord {
+    
+    convenience init(bookclub: Bookclub)  {
+        self.init(recordType: BookclubConstants.recordTypeKey)
+        
+        self.setValuesForKeys([
+            BookclubConstants.nameKey : bookclub.name,
+            BookclubConstants.adminKey : bookclub.admin,
+            BookclubConstants.adminContactInfoKey: bookclub.adminContactInfo,
+            BookclubConstants.membersKey : bookclub.members,
+            BookclubConstants.descriptionKey : bookclub.description,
+            BookclubConstants.profilePictureKey : bookclub.profilePicture,
+            BookclubConstants.currentlyReadingKey : bookclub.currentlyReading,
+            BookclubConstants.pastReadsKey : bookclub.pastReads,
+            BookclubConstants.memberMessagesKey : bookclub.memberMessages,
+            BookclubConstants.meetingInfoKey : bookclub.meetingInfo,
+            BookclubConstants.memberCapacityKey : bookclub.memberCapacity
+        ])
+    }
+}
+
+extension Bookclub {
+    
+    convenience init?(ckRecord: CKRecord) {
+        guard let name = ckRecord[BookclubConstants.nameKey] as? String,
+            let admin = ckRecord[BookclubConstants.adminKey] as? [User],
+            let adminContactInfo = ckRecord[BookclubConstants.adminContactInfoKey] as? String,
+            let members = ckRecord[BookclubConstants.membersKey] as? [User],
+            let description = ckRecord[BookclubConstants.descriptionKey] as? String,
+            let profilePicture = ckRecord[BookclubConstants.profilePictureKey] as? UIImage,
+            let currentlyReading = ckRecord[BookclubConstants.currentlyReadingKey] as? [String],
+            let pastReads = ckRecord[BookclubConstants.pastReadsKey] as? [String],
+            let memberMessages = ckRecord[BookclubConstants.memberMessagesKey] as? [Message],
+            let meetingInfo = ckRecord[BookclubConstants.meetingInfoKey] as? String,
+            let memberCapacity = ckRecord[BookclubConstants.memberCapacityKey] as? Int else {return nil}
+        
+        self.init(name: name, admin: admin, adminContactInfo: adminContactInfo, members: members, description: description, profilePicture: profilePicture, currentlyReading: currentlyReading, pastReads: pastReads, memberMessages: memberMessages, meetingInfo: meetingInfo, memberCapacity: memberCapacity)
+        
+    }
+}
+
