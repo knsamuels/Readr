161a6caca50b6ae4a41861e4688446d8ad1a0f44
Kristin Samuels <krislanza@yahoo.com>
2020-08-10 12:26:32 -0400

    start BookshelfController

---
+++ Readr/Readr/Helpers/BookshelfError.swift
@@ -0,0 +1,29 @@
+//
+//  BookshelfError.swift
+//  Readr
+//
+//  Created by Kristin Samuels  on 8/10/20.
+//  Copyright © 2020 Kristin Samuels . All rights reserved.
+//
+
+import Foundation
+
+enum BookshelfError: LocalizedError {
+    
+    case ckError(Error)
+    case couldNotUnwarp
+    case unableToDeleteRecord
+    
+    var errorDescription: String {
+        switch self {
+        case .ckError(let error):
+            return error.localizedDescription
+        case . couldNotUnwarp :
+            return "Unable to unwarp this post"
+        case . unableToDeleteRecord:
+            return "Unable to delete record from CloudKit"
+        }
+    }
+}
+
+
