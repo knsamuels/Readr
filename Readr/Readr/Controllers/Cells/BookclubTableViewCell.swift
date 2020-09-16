96fa8c50ca3c4b6c24e3b448f94296ca2d146ad0
Kristin Samuels <krislanza@yahoo.com>
2020-09-16 16:34:16 -0400

    create custom cell for member and bookclub

---
+++ Readr/Readr/Controllers/Cells/BookclubTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BookclubTableViewCell.swift
+//  Readr
+//
+//  Created by Kristin Samuels  on 9/16/20.
+//  Copyright © 2020 Kristin Samuels . All rights reserved.
+//
+
+import UIKit
+
+class BookclubTableViewCell: UITableViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var bookclubImage: UIImageView!
+    @IBOutlet weak var bookclubLabel: UILabel!
+    
+    //MARK: - Properties
+    var bookclub: Bookclub? {
+        didSet{
+            updateViews()
+        }
+    }
+    
+    //MARK: - Helper Methods
+    func updateViews() {
+        guard let bookclub = bookclub else {return}
+        bookclubImage.image = bookclub.profilePicture
+        bookclubLabel.text = bookclub.name
+    }
+}
