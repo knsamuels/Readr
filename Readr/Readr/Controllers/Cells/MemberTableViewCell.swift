2267eedbcb7d1bee2897b17c4d6287cad934b1ef
Kristin Samuels <krislanza@yahoo.com>
2020-10-16 16:43:51 -0400

    update member list cell for option to block a user

--- Readr/Readr/Controllers/Cells/MemberTableViewCell.swift
+++ Readr/Readr/Controllers/Cells/MemberTableViewCell.swift
@@ -14,12 +14,20 @@ class MemberTableViewCell: UITableViewCell {
     
     @IBOutlet weak var memberImage: UIImageView!
     @IBOutlet weak var memberName: UILabel!
+    @IBOutlet weak var optionButton: UIButton!
+    @IBOutlet weak var dotsStackView: UIStackView!
     
     //MARK: - Properties
     var member: User? {
         didSet{
             updateViews()
         }
+    }
+    var bookclub: Bookclub?
+    
+    //MARK: _ Actions
+    @IBAction func optionButtonTapped(_ sender: Any) {
+    
     }
     
     //MARK: - Helper Methods
@@ -34,5 +42,52 @@ class MemberTableViewCell: UITableViewCell {
         }
         memberImage.layer.cornerRadius = memberImage.frame.height / 2
         memberImage.clipsToBounds = true
+    }
+    
+    func presentOptionAlert() {
+        guard let bookclub = bookclub,
+            let member = member else {return}
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let removeAction = UIAlertAction(title: "Remove", style: .default) { (_) in
+            print("remove")
+        }
+        let blockAction = UIAlertAction(title: "Block", style: .destructive) { (_) in
+            let confirmBlockController = UIAlertController(title: "Block User?", message: "You will never be able to unblock once you block.", preferredStyle: .alert)
+            let cancelBlockAction = UIAlertAction(title: "Cancel", style: .cancel)
+            let confirmBlockAction = UIAlertAction(title: "Block", style: .destructive) { (_) in
+                bookclub.blockedUsers.append(member.username)
+                
+                BookclubController.shared.update(bookclub: bookclub) { (result) in
+                    switch result {
+                    case .success(_):
+                        UserController.shared.updateUser(user: member) { (result) in
+                            DispatchQueue.main.async {
+                                switch result {
+                                case .success(_):
+                                    print("Success")
+                                case .failure(_):
+                                    print("could not update the user")
+                                }
+                            }
+                        }
+                    case .failure(_):
+                        print("could not update the bookclub")
+                    }
+                }
+            }
+            confirmBlockController.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.white
+            confirmBlockController.view.tintColor = .accentBlack
+            confirmBlockController.addAction(cancelBlockAction)
+            confirmBlockController.addAction(confirmBlockAction)
+//            self.present(confirmBlockController, animated: true)
+        }
+        alertController.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.white
+        alertController.view.tintColor = .accentBlack
+        alertController.addAction(cancelAction)
+        alertController.addAction(blockAction)
+        
+//        self.present(alertController, animated: true)
     }
 } //End of class
--- Readr/Readr/Controllers/View Controllers/MemberListTableViewController.swift
+++ Readr/Readr/Controllers/View Controllers/MemberListTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MemberListTableViewController: UITableViewController {
-
+    
     
     var bookclub: Bookclub?
     var bookclubMembers: [User] = []
@@ -18,76 +18,85 @@ class MemberListTableViewController: UITableViewController {
         super.viewDidLoad()
         fetchUsers()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         self.title = "Members"
         self.navigationController?.navigationBar.titleTextAttributes = [ NSAttributedString.Key.font: UIFont(name: "Cochin", size: 20.0)!]
-       self.navigationController?.navigationBar.tintColor = .black
-          self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        self.navigationController?.navigationBar.tintColor = .black
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
     }
     
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return bookclubMembers.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "memberCell", for: indexPath) as? MemberTableViewCell else {return UITableViewCell()}
+        guard let bookclub = bookclub else {return UITableViewCell()}
+        guard let currentUser = UserController.shared.currentUser else {return UITableViewCell()}
+        if bookclub.admin != currentUser.appleUserRef {
+            cell.optionButton.isHidden = true
+            cell.dotsStackView.isHidden = true
+        } else {
+            cell.optionButton.isHidden = false
+            cell.dotsStackView.isHidden = false
+        }
         let member = bookclubMembers[indexPath.row]
         cell.member = member
-    
+        //        cell.bookclub = bookclub
         return cell
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     //MARK: - Helper Functions
     func fetchUsers() {
         guard let bookclub = bookclub else {return}
         
-//        BookclubController.shared.fetchMembers(ofBookclub: bookclub) { (result) in
-//            switch result {
-//            case .success(let members):
-//                self.bookclubMembers = members
-//                self.tableView.reloadData()
-//            case .failure(_):
-//                print("FAILED!!")
-//            }
-//        }
+        //        BookclubController.shared.fetchMembers(ofBookclub: bookclub) { (result) in
+        //            switch result {
+        //            case .success(let members):
+        //                self.bookclubMembers = members
+        //                self.tableView.reloadData()
+        //            case .failure(_):
+        //                print("FAILED!!")
+        //            }
+        //        }
         
         for reference in bookclub.members {
             UserController.shared.fetchUser(withReference: reference) { (result) in
@@ -105,7 +114,7 @@ class MemberListTableViewController: UITableViewController {
     }
     
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "memberListToUser" {
             guard let destination = segue.destination as?
