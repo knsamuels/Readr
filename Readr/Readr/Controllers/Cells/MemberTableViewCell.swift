7ab75efd1af213a530d314e726d4fad55ae3b0ba
Kristin Samuels <krislanza@yahoo.com>
2020-10-19 15:28:18 -0400

    add ability to block and remove user from bookclub

--- Readr/Readr/Controllers/Cells/MemberTableViewCell.swift
+++ Readr/Readr/Controllers/Cells/MemberTableViewCell.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol BlockMemberDelegate: AnyObject {
+    func presentBlockAlert(member: User)
+}
+
 class MemberTableViewCell: UITableViewCell {
     
     //MARK: - Outlets
@@ -24,10 +28,12 @@ class MemberTableViewCell: UITableViewCell {
         }
     }
     var bookclub: Bookclub?
+    weak var blockDelegate: BlockMemberDelegate?
     
     //MARK: _ Actions
     @IBAction func optionButtonTapped(_ sender: Any) {
-    
+        guard let member = member else {return}
+        blockDelegate?.presentBlockAlert(member: member)
     }
     
     //MARK: - Helper Methods
@@ -42,52 +48,5 @@ class MemberTableViewCell: UITableViewCell {
         }
         memberImage.layer.cornerRadius = memberImage.frame.height / 2
         memberImage.clipsToBounds = true
-    }
-    
-    func presentOptionAlert() {
-        guard let bookclub = bookclub,
-            let member = member else {return}
-        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        let removeAction = UIAlertAction(title: "Remove", style: .default) { (_) in
-            print("remove")
-        }
-        let blockAction = UIAlertAction(title: "Block", style: .destructive) { (_) in
-            let confirmBlockController = UIAlertController(title: "Block User?", message: "You will never be able to unblock once you block.", preferredStyle: .alert)
-            let cancelBlockAction = UIAlertAction(title: "Cancel", style: .cancel)
-            let confirmBlockAction = UIAlertAction(title: "Block", style: .destructive) { (_) in
-                bookclub.blockedUsers.append(member.username)
-                
-                BookclubController.shared.update(bookclub: bookclub) { (result) in
-                    switch result {
-                    case .success(_):
-                        UserController.shared.updateUser(user: member) { (result) in
-                            DispatchQueue.main.async {
-                                switch result {
-                                case .success(_):
-                                    print("Success")
-                                case .failure(_):
-                                    print("could not update the user")
-                                }
-                            }
-                        }
-                    case .failure(_):
-                        print("could not update the bookclub")
-                    }
-                }
-            }
-            confirmBlockController.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.white
-            confirmBlockController.view.tintColor = .accentBlack
-            confirmBlockController.addAction(cancelBlockAction)
-            confirmBlockController.addAction(confirmBlockAction)
-//            self.present(confirmBlockController, animated: true)
-        }
-        alertController.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.white
-        alertController.view.tintColor = .accentBlack
-        alertController.addAction(cancelAction)
-        alertController.addAction(blockAction)
-        
-//        self.present(alertController, animated: true)
     }
 } //End of class
